943fb3aa7e65fd25e8df191dbcf87a813eb79642
lhmcgann <hailey.mcgann@gmail.com>
2020-04-10 12:42:11 -0700

    Submit button

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter}/>
-        <Form />
+        <Form handleSubmit={this.handleSubmit}/>
       </div>
     )
   }
@@ -26,6 +26,10 @@ class App extends Component {
       }),
     })
   }
+
+  handleSubmit = character => {
+     this.setState({ characters: [...this.state.characters, character] })
+   }
 }
 
 
